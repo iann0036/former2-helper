adae76183434a33b21207813cdec0dee50e334e8
Ian Mckay <ian.mckay@kablamo.com.au>
2019-03-03 23:37:22 +1100

    Respond to ping

--- bg.js
+++ bg.js
@@ -1,16 +1,9 @@
-/*function constructServiceByName(name, props) {
-    if (name == "Lambda")
-        return new AWS.Lambda(props);
-    if (name == "S3")
-        return new AWS.S3(props);
-    
-    throw "Unsupported Service";
-}*/
-
 chrome.runtime.onMessageExternal.addListener(
     function(request, sender, sendResponse) {
         if (["127.0.0.1", "former2.com", "www.former2.com"].includes(new URL(sender.url).hostname)) {
-            if (request.action == "configUpdate") {
+            if (request.action == "ping") {
+                sendResponse(true);
+            } else if (request.action == "configUpdate") {
                 request.obj.credentials = new AWS.Credentials(
                     request.obj.credentials[0],
                     request.obj.credentials[1],
@@ -25,8 +18,6 @@ chrome.runtime.onMessageExternal.addListener(
                 });
             } else if (request.action == "serviceAction") {
                 var svc = new AWS[request.service.name](request.service.properties);
-                
-                //var svc = constructServiceByName(request.service.name, request.service.properties);
 
                 svc[request.service_action](request.params, function(err, data) {
                     if (err) {
