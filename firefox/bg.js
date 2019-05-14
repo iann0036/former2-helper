7aa63d139b91ca2630a613a8cc2c4454cf743201
Ian Mckay <contact@ianmckay.com.au>
2019-05-14 20:42:52 +1000

    Add localhost to supported source hostnames #1

--- firefox/bg.js
+++ firefox/bg.js
@@ -1,6 +1,6 @@
 browser.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (["127.0.0.1", "former2.com", "www.former2.com"].includes(new URL(sender.url).hostname)) {
+        if (["127.0.0.1", "localhost", "former2.com", "www.former2.com"].includes(new URL(sender.url).hostname)) {
             if (request.action == "ping") {
                 sendResponse({
                     'success': true,
