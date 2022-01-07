219fe422641041105644314607e67ee99b618586
Ian Mckay <contact@ianmckay.com.au>
2022-01-07 15:56:25 +1100

    Update helper to manifest v3

--- chrome/bg.js
+++ chrome/bg.js
@@ -1,5 +1,7 @@
+importScripts("aws-sdk-2.1051.0-sw.js");
+
 chrome.runtime.onMessageExternal.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
         if (["127.0.0.1", "localhost", "former2.com", "www.former2.com"].includes(new URL(sender.url).hostname)) {
             if (request.action == "ping") {
                 sendResponse({
@@ -21,9 +23,19 @@ chrome.runtime.onMessageExternal.addListener(
                 });
             } else if (request.action == "serviceAction") {
                 try {
+                    if (request.config) {
+                        request.config.credentials = new AWS.Credentials(
+                            request.config.credentials[0],
+                            request.config.credentials[1],
+                            request.config.credentials[2]
+                        );
+        
+                        AWS.config.update(request.config);
+                    }
+
                     var svc = new AWS[request.service.name](request.service.properties);
 
-                    svc[request.service_action](request.params, function(err, data) {
+                    svc[request.service_action](request.params, function (err, data) {
                         if (err) {
                             sendResponse({
                                 'success': false,
@@ -37,7 +49,7 @@ chrome.runtime.onMessageExternal.addListener(
                             });
                         }
                     });
-                } catch(err) {
+                } catch (err) {
                     sendResponse({
                         'success': false,
                         'error': `The call to the SDK failed (${request.service.name}.${request.service_action}). You may need to update the Former2 Helper extension.`,
